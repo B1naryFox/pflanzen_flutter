3b28fb09e9ff0ca0f57170fbf203e9bd3a99b771
Elinor Armitage <armitage.elinor@outlook.de>
2026-04-15 09:40:28 +0200

    refactor

--- lib/data/plantRepository.dart
+++ lib/data/plantRepository.dart
@@ -14,23 +14,23 @@ class PlantRepository {
 
   // -- CRUD --
   Future<List<Plant>> loadPlants() async {
-    final db = await db_service.database;
-    final List<Map<String, dynamic>> plantMaps = await db.query(db_service.DBTABLENAME);
+    final db = await dbService.database;
+    final List<Map<String, dynamic>> plantMaps = await db.query(dbService.DBTABLENAME);
     return List.generate(plantMaps.length, (index) => Plant.fromMap(plantMaps[index]));
   }
   
   Future<void> addPlant(Plant plant) async {
-     final db = await db_service.database;
-     await db.insert(db_service.DBTABLENAME, plant.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+     final db = await dbService.database;
+     await db.insert(dbService.DBTABLENAME, plant.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
   Future<void> updatePlant(Plant plant) async {
-    final db = await db_service.database;
-    await db.update(db_service.DBTABLENAME, plant.toMap(), where: 'id =?', whereArgs: [plant.id]);
+    final db = await dbService.database;
+    await db.update(dbService.DBTABLENAME, plant.toMap(), where: 'id =?', whereArgs: [plant.id]);
   }
 
   Future<void> deletePlant(String id) async {
-    final db = await db_service.database;
+    final db = await dbService.database;
     await db.delete(
         dbService.DBTABLENAME, where: 'id =?',
         whereArgs: [id]);
